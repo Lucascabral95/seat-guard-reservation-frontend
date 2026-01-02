8590798641d409fbf1009bc31a5e53f7be04576a
Lucas Cabral <lucassimple@hotmail.com>
2026-01-02 15:25:20 -0300

    fix(vercel): fix PDF opening issue

--- src/app/features/checkout/components/actions-buttons/component-actions-buttons/component-actions-buttons.ts
+++ src/app/features/checkout/components/actions-buttons/component-actions-buttons/component-actions-buttons.ts
@@ -1,5 +1,8 @@
-import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, EventEmitter, inject, Input, Output, signal } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { environment } from '../../../../../../environments/environment';
+
+const URL_BOOKING_SERVICE = environment.apiBookingServiceUrl;
 
 @Component({
   selector: 'component-actions-buttons',
@@ -11,4 +14,12 @@ export default class ComponentActionsButtons {
      @Input({ required: true }) order_id!: string;
      @Input({ required: true }) isLoadingPdf = signal<boolean>(false);
      @Output() viewTicket = new EventEmitter<void>();
+     router = inject(Router)
+
+     viewPDF() {
+       if (!this.order_id) return;
+       const externalUrl = `${URL_BOOKING_SERVICE}/api/v1/tickets/${this.order_id}/download`;
+
+       window.open(externalUrl, '_blank');
+     }
 }
--- src/app/shared/utils/open-pdf.utils.ts
+++ src/app/shared/utils/open-pdf.utils.ts
@@ -7,76 +7,31 @@ interface OpenPdfParams {
   setError: (message: string | null) => void;
 }
 
-// export function openPdf({
-//   orderId,
-//   download$,
-//   setLoading,
-//   setError
-// }: OpenPdfParams) {
-//   if (!orderId) {
-//     setError('No se pudo obtener el ID de la orden');
-//     setLoading(false);
-//     return;
-//   }
-
-//   setLoading(true);
-//   setError(null);
-
-//   download$(orderId).subscribe({
-//     next: (blob) => {
-//       const url = URL.createObjectURL(blob);
-
-//       window.open(url, '_blank');
-
-//       setLoading(false);
-//     },
-//     error: () => {
-//       setError('Error al abrir el ticket');
-//       setLoading(false);
-//     }
-//   });
-// }
-
-// shared/utils/open-pdf.utils.ts
-
 export function openPdf({
   orderId,
   download$,
   setLoading,
   setError
 }: OpenPdfParams) {
-  if (!orderId) return;
+  if (!orderId) {
+    setError('No se pudo obtener el ID de la orden');
+    setLoading(false);
+    return;
+  }
 
   setLoading(true);
+  setError(null);
 
   download$(orderId).subscribe({
-    next: (data) => {
-      // 1. Crear el Blob
-      const blob = new Blob([data], { type: 'application/pdf' });
+    next: (blob) => {
       const url = URL.createObjectURL(blob);
 
-      // 2. ABRIR EN PESTAÑA (Sin descargar)
-      // La clave es NO poner el atributo 'download'
-      const link = document.createElement('a');
-      link.href = url;
-      link.target = '_blank'; // Abrir en nueva pestaña
-      // link.download = ...; <--- ¡BORRA ESTA LÍNEA! Si la dejas, se descarga y deja la pestaña en blanco.
-
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-
-      // 3. Limpieza diferida (importante no hacerlo muy rápido)
-      // Esperamos 1 minuto para asegurar que el PDF cargó en la otra pestaña
-      setTimeout(() => {
-        window.URL.revokeObjectURL(url);
-      }, 60000);
+      window.open(url, '_blank');
 
       setLoading(false);
     },
-    error: (err) => {
-      console.error(err);
-      setError('No se pudo abrir el ticket.');
+    error: () => {
+      setError('Error al abrir el ticket');
       setLoading(false);
     }
   });

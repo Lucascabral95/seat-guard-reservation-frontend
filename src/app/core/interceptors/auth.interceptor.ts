572cb13a6bb59a238c6ed9f038576d85cbc941cd
Lucas Cabral <lucassimple@hotmail.com>
2026-01-03 02:15:47 -0300

    docs(readme): add professional project documentation

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,69 +1,39 @@
-/////////////// Funciona en desarrollo
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { environment } from '../../../environments/environment';
 
 const TOKEN_LS_KEY = environment.localStorage;
-const SECRET_X_INTERNAL = environment.xInternalSecret
+const SECRET_X_INTERNAL = environment.xInternalSecret;
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const platformId = inject(PLATFORM_ID);
   const isBrowser = isPlatformBrowser(platformId);
 
-  if (!isBrowser) {
-    return next(req);
+  let headers = req.headers.set('X-Internal-Secret', SECRET_X_INTERNAL);
+  let url = req.url;
+
+  if (isBrowser) {
+    const token = localStorage.getItem(TOKEN_LS_KEY);
+
+    if (token) {
+      headers = headers.set('Authorization', `Bearer ${token}`);
+    }
+
+    if (
+      environment.production &&
+      url.startsWith('http://') &&
+      !url.includes('localhost') &&
+      !url.includes('127.0.0.1')
+    ) {
+      url = `/proxy?target=${encodeURIComponent(url)}`;
+    }
   }
 
-  const token = localStorage.getItem(TOKEN_LS_KEY);
-
-  if (token) {
-    const clonedRequest = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-        'X-Internal-Secret': SECRET_X_INTERNAL,
-      }
-    });
-    return next(clonedRequest);
-  }
-
-  return next(req);
+  return next(
+    req.clone({
+      url,
+      headers,
+    })
+  );
 };
-
-////////// Funciona en produccion
-// import { HttpInterceptorFn } from '@angular/common/http';
-// import { inject, PLATFORM_ID } from '@angular/core';
-// import { isPlatformBrowser } from '@angular/common';
-// import { environment } from '../../../environments/environment';
-
-// const TOKEN_LS_KEY = environment.localStorage;
-// const SECRET_X_INTERNAL = environment.xInternalSecret;
-
-// export const authInterceptor: HttpInterceptorFn = (req, next) => {
-//   const platformId = inject(PLATFORM_ID);
-//   const isBrowser = isPlatformBrowser(platformId);
-
-//   let headers = req.headers;
-//   if (isBrowser) {
-//     const token = localStorage.getItem(TOKEN_LS_KEY);
-//     if (token) {
-//       headers = headers.set('Authorization', `Bearer ${token}`);
-//     }
-//   }
-//   headers = headers.set('X-Internal-Secret', SECRET_X_INTERNAL);
-
-//   let url = req.url;
-
-//   if (isBrowser && url.startsWith('http://') && !url.includes('localhost || 127.0.0.1')) {
-//      console.log('Interceptor: Redirigiendo URL insegura a Proxy ->', url);
-//      url = `/proxy?target=${encodeURIComponent(url)}`;
-//   }
-
-//   const clonedRequest = req.clone({
-//     url: url,
-//     headers: headers
-//   });
-
-//   return next(clonedRequest);
-// };
-
